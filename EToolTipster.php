14a4f22aac1bbcff1900d57f142f4a00303f799e
Ibrar Hussain <ibrarturi@gmail.com>
2014-02-13 16:00:36 +0500

    committing extenion files

---
+++ EToolTipster.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Wrapper for Jquery ToolTipster
+ *
+ * @author ibrar
+ */
+class EToolTipster extends CWidget {
+
+    /**
+     * 
+     * @var string CSS class name or Element Id 
+     */
+    public $target = '.tooltip';
+    
+    /**
+     *
+     * @var array options array to pass jquery plugin 
+     */
+    public $options = array();
+
+    /**
+     * Init widget
+     */
+    public function init() {
+        parent::init();
+
+        $this->registerClientScript();
+    }
+
+    protected function registerClientScript() {
+        $cs = Yii::app()->getClientScript();
+        $scriptUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('application.extensions.etooltipster.resources'));
+
+        $cs->registerCssFile($scriptUrl . '/css/tooltipster.css');
+        $cs->registerCssFile($scriptUrl . '/css/themes/tooltipster-light.css');
+        $cs->registerCssFile($scriptUrl . '/css/themes/tooltipster-noir.css');
+        $cs->registerCssFile($scriptUrl . '/css/themes/tooltipster-punk.css');
+        $cs->registerCssFile($scriptUrl . '/css/themes/tooltipster-shadow.css');
+        $cs->registerCoreScript('jquery');
+        $cs->registerScriptFile($scriptUrl . '/js/jquery.tooltipster.min.js', CClientScript::POS_END);
+    }
+
+    public function run() {
+        $options = (isset($this->options)) ? $this->options : array();
+        
+        $js = "$('{$this->target}').tooltipster(".CJavaScript::encode($options).");";
+        
+        $cs = Yii::app()->clientScript;
+        $cs->registerScript('tooltipster',$js, CClientScript::POS_READY);
+    }
+}
+
+?>
